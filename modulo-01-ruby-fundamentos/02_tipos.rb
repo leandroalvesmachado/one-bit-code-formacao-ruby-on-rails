baf28e66bd14a1387bc97cce864f8719eb1b40c6
leandroalvesmachado <leandroalvesmachado@gmail.com>
2024-04-22 08:53:18 -0300

    salvando

---
+++ modulo-01-ruby-fundamentos/02_tipos.rb
@@ -0,0 +1,11 @@
+# Dados do Filme
+name = 'Avatar'
+year_launch = 2022
+price = 100.00
+plan_included = false
+
+puts name, year_launch
+puts name.class # String
+puts year_launch.class # Integer
+puts price.class # Float
+puts plan_included.class # FalseClass
